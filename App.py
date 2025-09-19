c3d6d95ff2b42836c82ec705f4e41eced6642f53
Wyndam Baxter <84729993+WyndamBaxter@users.noreply.github.com>
2025-09-19 00:57:56 -0300

    Criação da primeira linha dos dados gerais

--- App.py
+++ App.py
@@ -18,15 +18,21 @@ def load_data():
 
 df_emprestimos_completo = load_data()
 
+# --- Diconário para nomes de mêses
+
+meses_nomes = {
+     1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril',
+    5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto',
+    9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
+}
+
+
 # --- Sidebar (Filtros) ---
 st.sidebar.header("🔎Filtros")
 
 # Filtro de Ano 
 anos_disponiveis = sorted(df_emprestimos_completo.data_emprestimo.dt.year.unique())
 anos_selecionados = st.sidebar.multiselect('Ano', anos_disponiveis, default=anos_disponiveis) 
-
-# Filtro 
-
 
 # Filtragem do dataframe
 df_filtrado = df_emprestimos_completo[
@@ -35,7 +41,7 @@ df_filtrado = df_emprestimos_completo[
 
 # --- Conteúdo Principal ---
 
-st.title("Dashboard de análises da biblioteca SISBI")
+st.title("Dashboard de análise dos empréstimos da biblioteca SISBI")
 st.markdown("Analise bem...")
 
 # --- Métricas Gerais ---
@@ -44,12 +50,15 @@ st.subheader('Métricas Gerais')
 if not df_filtrado.empty:
     emprestimos = len(df_filtrado.id_emprestimo.unique())
     exemplares = len(df_filtrado.id_emprestimo)
-    mes_emprestimos_maximo = df_filtrado
+    mes_numero = df_filtrado.data_emprestimo.dt.month.value_counts().idxmax()
+    mes_nome = meses_nomes.get(mes_numero, 'Desconhecido')
+    base_mais_acessada = df_filtrado.biblioteca.value_counts().idxmax()
 else:
-    emprestimos, exemplares = 0, 0
+    emprestimos, exemplares, mes_emprestimos_maximo, base_mais_acessada = 0, 0, 'Nenhum', 'Nenhuma'
 
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Total de Empréstimos", emprestimos)
 col2.metric('Total de exemplares', exemplares)
-col3.metric('Mês com maior número de empréstimos', exemplares)
-col4.metric('Mês com menor número de empréstimos', exemplares)
+col3.metric('Mês com maior número de empréstimos', mes_nome)
+col4.metric('Base mais Acessada', base_mais_acessada)
+
