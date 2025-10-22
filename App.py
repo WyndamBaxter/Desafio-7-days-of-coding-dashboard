b7bbd8cd3931db598d1ae41c210c55b1e2769475
Wyndam Baxter <84729993+WyndamBaxter@users.noreply.github.com>
2025-10-21 23:30:41 -0300

    Alterações de diretorio

--- App.py
+++ App.py
@@ -14,8 +14,8 @@ st.set_page_config(
 # --- Carregamento de dados ---
 @st.cache_data
 def load_data():
-    df = pd.read_parquet('F:/Programação/Desafio-7-days-of-coding-dashboard/resultados/df_emprestimos_completo.parquet')
-    #df = pd.read_parquet('C:/Users/PC/Desktop/Desafio-7-days-of-coding-dashboard/resultados/df_emprestimos_completo.parquet')
+    #df = pd.read_parquet('F:/Programação/Desafio-7-days-of-coding-dashboard/resultados/df_emprestimos_completo.parquet')
+    df = pd.read_parquet('df_emprestimos_completo.parquet')
     return df
 
 df_emprestimos_completo = load_data()
